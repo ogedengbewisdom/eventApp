ff441da30aeea4338d0be17926c6b91f7804cf06
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-19 17:52:54 +0100

    RootLayout route

---
+++ frontend/src/pages/Root.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react"
+import MainNavigation from "../components/MainNavigation"
+import { Outlet } from "react-router-dom"
+
+
+const RootLayout = () => {
+    return (
+        <Fragment>
+            <MainNavigation />
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default RootLayout
