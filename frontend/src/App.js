ebdf9559f7285d1f3860703389bb506127484a21
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-20 20:38:00 +0100

    Data fetching with a loader prop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,4 @@
 
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import HomePage from "./pages/HomePage";
-import EventsPage from "./pages/EventsPage";
-import NewEventPage from "./pages/NewEventPage";
-import EventDetailPage from "./pages/EventDetailPage";
-import EditEventPage from "./pages/EditEventPage";
-import RootLayout from "./pages/Root";
-import EventRootLayout from "./pages/EventRoot";
-
-function App() {
-
-  const router = createBrowserRouter([
-    { path: "/", element: <RootLayout />, 
-    
-    children: [
-
-      {index: true, element: <HomePage />},
-      {path: "events", element: <EventRootLayout />, children: [
-        {index: true, element: <EventsPage />},
-        {path: ":eventId", element: <EventDetailPage />},
-        {path: "new", element: <NewEventPage />},
-        {path: ":eventId/edit", element: <EditEventPage />}
-      ]},
-    ]}
-
-
-  ])
-
-  return (
-    <RouterProvider router={router} />
-  );
-}
-
-export default App;
-
-
-
 // Challenge / Exercise
 
 // 1. Add five new (dummy) page components (content can be simple <h1> elements)
@@ -57,3 +20,53 @@ export default App;
 //    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import HomePage from "./pages/HomePage";
+import EventsPage from "./pages/EventsPage";
+import NewEventPage from "./pages/NewEventPage";
+import EventDetailPage from "./pages/EventDetailPage";
+import EditEventPage from "./pages/EditEventPage";
+import RootLayout from "./pages/Root";
+import EventRootLayout from "./pages/EventRoot";
+
+function App() {
+
+  const router = createBrowserRouter([
+    { path: "/", element: <RootLayout />, 
+    
+    children: [
+
+      {index: true, element: <HomePage />},
+      {
+        path: "events", 
+        element: <EventRootLayout />, 
+        children: [
+        {
+          index: true, 
+          element: <EventsPage />, 
+          loader: async() => {
+          const response = await fetch('http://localhost:8080/events')
+          if (!response.ok) {
+            //.....
+          } else {
+            const resData = await response.json()
+            return resData.events
+          } 
+ 
+        }},
+        {path: ":eventId", element: <EventDetailPage />},
+        {path: "new", element: <NewEventPage />},
+        {path: ":eventId/edit", element: <EditEventPage />}
+      ]},
+    ]}
+
+
+  ])
+
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
