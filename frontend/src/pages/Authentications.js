bbeb262cfcb6b926732f4e31bc8c45656b26c09d
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-05-01 18:58:30 +0100

    implementing the auth action

---
+++ frontend/src/pages/Authentications.js
@@ -0,0 +1,42 @@
+import { json, redirect } from "react-router-dom"
+import AuthForm from "../components/AuthForm"
+
+
+const AuthenticationPage = () => {
+    return(
+        <AuthForm />
+    )
+}
+
+export default AuthenticationPage
+
+export const action = async ({request}) => {
+    const searchParams = new URL(request.url).searchParams
+    const mode = searchParams.get("mode") || "login"
+    if (mode !== "login" && mode !== "signup") {
+        throw json({message: "Unsupported mode!"}, {status: 422})
+    }
+    const data = await request.formData()
+    const authData = {
+        email: data.get("email"),
+        password: data.get("password")
+    }
+
+    const response = await fetch(`http://localhost:8080/${mode}`, {
+        method: "POST",
+        body: JSON.stringify(authData),
+        headers: {
+            "Content-Type" : "application/json"
+        }
+    })
+
+    if (response.status === 401 || response.status === 422) {
+        return response
+    }
+
+    if (!response.ok) {
+        throw json({message: "Could not authenticate user"}, {status: 500})
+    }
+
+    return redirect("/")
+}
