9b9a594f032649db17f2478a6e0e64907a064181
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-05-02 13:58:09 +0100

    adding logout to the mainNavigaion navbar

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,8 +1,16 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useRouteLoaderData, useSubmit } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
 import NewsletterSignup from './NewsletterSignup';
 
 function MainNavigation() {
+  const isAuth = useRouteLoaderData("root")
+  const submit = useSubmit()
+  const logoutHandler = () => {
+    const proceed = window.confirm("Are you sure you want to logout ?")
+    if (proceed) {
+      submit(null, {method: "post", action: "logout"})
+    }
+  }
   return (
     <header className={classes.header}>
       <nav>
@@ -16,9 +24,12 @@ function MainNavigation() {
           <li>
             <NavLink className={ ({isActive}) => isActive ? classes.active : undefined} to='/newsletter'>Newsletter</NavLink>
           </li>
-          <li>
+          {!isAuth && <li>
             <NavLink className={ ({isActive}) => isActive ? classes.active : undefined} to='/auth?mode=login'>Authentication</NavLink>
-          </li>
+          </li>}
+          {isAuth && 
+          <button onClick={logoutHandler}>Logout</button>
+        }
         </ul>
       </nav>
       <NewsletterSignup />
