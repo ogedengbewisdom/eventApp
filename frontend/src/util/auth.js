30dad020084e153c0f164342795b384de915df76
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-05-02 01:43:07 +0100

    managing authorization action

---
+++ frontend/src/util/auth.js
@@ -0,0 +1,21 @@
+import { redirect } from "react-router-dom";
+
+
+export const getAuthToken = () => {
+   const token = localStorage.getItem("token");
+   return token
+}
+
+
+export const tokenLoader = () => {
+    return getAuthToken()
+}
+
+export const checkAuthLoader = () => {
+    const token = getAuthToken()
+    if (!token) {
+        return redirect("/auth")
+    }
+
+    return null
+}
