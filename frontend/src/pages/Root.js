3939fcadfe630fe95d3f8ba8d472558df7aa80d8
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-05-02 13:55:25 +0100

    logout authomatically on expiration of the token

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,10 +1,28 @@
-import { Fragment } from "react"
+import { Fragment, useEffect } from "react"
 import MainNavigation from "../components/MainNavigation"
-import { Outlet } from "react-router-dom"
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom"
+import { getTokenDuration } from "../util/auth"
 
 
 const RootLayout = () => {
     // const navigate = useNavigation()
+    const submit = useSubmit()
+    const token = useLoaderData()
+    useEffect( () => {
+        if (!token) {
+            return
+        }
+        if (token === "EXPIRED") {
+            submit(null, {action: "/logout", method: "post"});
+            return
+        }
+        const tokenDuration = getTokenDuration()
+        console.log(tokenDuration)
+        
+        setTimeout( () => {
+            submit(null, {action: "/logout", method: "post"})
+        }, tokenDuration)
+    },[token, submit])
     
     return (
         <Fragment>
