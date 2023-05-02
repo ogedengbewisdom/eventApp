40e69ccde37f73188d4cb654a266ea87b3d96704
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-05-02 01:44:20 +0100

    App revaluation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,4 @@
 
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import HomePage from "./pages/HomePage";
 import EventsPage, { loader as eventsLoader} from "./pages/EventsPage";
@@ -33,6 +11,8 @@ import ErrorPage from "./pages/ErrorPage";
 import {action as maniulatAction} from "./components/EventForm"
 import NewsletterPage, {action as newsletterAction} from "./pages/Newsletter";
 import AuthenticationPage, {action as authAction} from "./pages/Authentications";
+import { action as logoutAction } from "./pages/Logout"
+import { checkAuthLoader, tokenLoader } from "./util/auth";
 
 function App() {
 
@@ -40,6 +20,8 @@ function App() {
     { 
       path: "/", 
       element: <RootLayout />,
+      id: "root",
+      loader: tokenLoader,
       errorElement: <ErrorPage />, 
     
     children: [
@@ -61,7 +43,7 @@ function App() {
             loader: eventDetailLoader,
             children: [
               {index: true, element: <EventDetailPage />, action: deleteEventAction},
-              {path: "edit", element: <EditEventPage />, action: maniulatAction}
+              {path: "edit", element: <EditEventPage />, action: maniulatAction, loader: checkAuthLoader}
             ]
           },
         {
@@ -71,10 +53,12 @@ function App() {
         {
           path: "new", 
           element: <NewEventPage />,
-          action: maniulatAction  
+          action: maniulatAction,
+          loader: checkAuthLoader 
         },
       ]},
-      {path: "newsletter", element: <NewsletterPage />, action: newsletterAction}
+      {path: "newsletter", element: <NewsletterPage />, action: newsletterAction},
+      {path: "logout", action: logoutAction}
     ]}
 
 
