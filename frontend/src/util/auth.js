bda59816a58ef6bda8fc5a2f8dde7069d9f2b85e
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-05-03 20:51:46 +0100

    set authomatic logout after expiration time

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,37 +1,37 @@
 import { redirect } from "react-router-dom";
 
 export const getTokenDuration = () => {
-    const storedTime = localStorage.getItem("expiration")
-    const loginTime = new Date(storedTime)
-    const now = new Date()
-    const duration = loginTime.getTime() - now.getTime()
-    return duration
+    const storedTime = localStorage.getItem("expiration");
+    const loginTime = new Date(storedTime);
+    const now = new Date();
+    const duration = loginTime.getTime() - now.getTime();
+    return duration;
 }
 
 export const getAuthToken = () => {
    const token = localStorage.getItem("token");
 
    if (!token) {
-    return null
+    return null;
    }
    
    const tokenDuration = getTokenDuration()
    if (tokenDuration < 0) {
-    return 'EXPIRED'
+    return 'EXPIRED';
    }
 
-   return token
+   return token;
 }
 
 export const tokenLoader = () => {
-    const token = getAuthToken()
-    return token
+    const token = getAuthToken();
+    return token;
 }
 
 export const checkAuthLoader = () => {
-    const token = getAuthToken()
+    const token = getAuthToken();
     if (!token) {
-        return redirect("/auth")
+        return redirect("/auth");
     }
-    return token
+    return token;
 }
