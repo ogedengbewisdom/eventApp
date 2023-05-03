789f53f1aa4d1fdb503752221c9df27b6e884852
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-05-03 20:47:40 +0100

    Error handling with custom Errors

--- frontend/src/pages/ErrorPage.js
+++ frontend/src/pages/ErrorPage.js
@@ -7,16 +7,16 @@ const ErrorPage = () => {
 
     const error = useRouteError()
 
-    let title = "An Error Occured!"
-    let message = "Something went wrong"
+    let title = "An Error Occured!";
+    let message = "Something went wrong";
 
     if (error.status === 500) {
-        message = error.data.message
+        message = error.data.message;
     }
 
     if (error.status === 404) {
-        title = "Not Found"
-        message = "Could not find resource or page"
+        title = "Not Found";
+        message = "Could not find resource or page";
     }
 
 
@@ -30,4 +30,4 @@ const ErrorPage = () => {
     )
 }
 
-export default ErrorPage
+export default ErrorPage;
