181bf951cbddbba9ea19e1a928202913ccc45e40
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-06-11 10:48:55 +0100

    added framer animation

--- frontend/src/pages/NewEventPage.js
+++ frontend/src/pages/NewEventPage.js
@@ -1,9 +1,16 @@
 
 import EventForm from "../components/EventForm"
+import { motion } from "framer-motion";
 
 const NewEventPage = () => {
     return (
-        <EventForm method="post" />
+        <motion.div 
+        initial={{y: "100vw"}}
+        animate={{y: 0}}
+        transition={{duration: 1}}
+        >
+            <EventForm method="post" />
+        </motion.div>
     )
 }
 
