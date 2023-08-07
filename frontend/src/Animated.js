50032f19141cfd8c5416f72df3923a7b00baacbf
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-08-07 17:23:48 +0100

    Animated

--- frontend/src/Animated.js
+++ frontend/src/Animated.js
@@ -9,17 +9,17 @@ function Animated() {
   const location = useLocation()
 
   return (
-    <AnimatePresence>
+    <AnimatePresence exitBeforeEnter>
       <motion.div
         location={location}
         key={location.pathname}
         initial="initial"
         animate="animate"
-        exit="exit"
+        exit= { {opacity: 0}}
         variants={{
           initial: { opacity: 0 },
           animate: { opacity: 1 },
-          exit: { opacity: 0 },
+          
           transition: {duration: 3, type: "spring", stiffness: 50}
         }}
       >
